df241dd44815a384b37a22e7beaf24dcbfff6718
mrivera <matias.g.rivera.civ@gmail.com>
2024-07-22 10:49:40 -0700

    Initialize AspNetCoreTodo

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {  
+        public IActionResult Index()
+        {
+            // Get todo items from database
+
+            // Put items into model
+
+            // Render view using the model
+        }
+    }
+}
