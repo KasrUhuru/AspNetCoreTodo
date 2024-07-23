4c545a2d48eb33e2a587e982cf88c8aeb330aace
mrivera <matias.g.rivera.civ@gmail.com>
2024-07-23 14:17:14 -0700

    Completed ASP.NET Core Book up to page 63 with no errors

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,19 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {  
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index()
         {
             // Get todo items from database
+            var items = await _todoItemService.GetIncompleteItemsAsync();
 
             // Put items into model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
 
             // Render view using the model
+            return View(model);
         }
     }
 }
--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -1,12 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
 using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodo.Services
 {
-    public interface ITodoItemService
+    public class TodoItemService : ITodoItemService
     {
-        Task<TodoItem[]> GetIncompleteItemAsync();
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            return await _context.Items
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
+        }
     }
 }
