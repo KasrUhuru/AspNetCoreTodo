d6e660c9c8e87e9877318a2b064297e9015817a2
MRivera <matias.g.rivera.civ@gmail.com>
2024-07-23 23:20:19 -0700

    Initial commit after making changes

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -16,6 +16,24 @@ namespace AspNetCoreTodo.Controllers
         {
             _todoItemService = todoItemService;
         }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var successful = await _todoItemService.AddItemAsync(newItem);
+            if (!successful)
+            {
+                return BadRequest(new{error="Could not add item."});
+            }
+
+            return RedirectToAction("Index");
+        }
+
         public async Task<IActionResult> Index()
         {
             // Get todo items from database
--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -23,5 +23,20 @@ namespace AspNetCoreTodo.Services
                 .Where(x => x.IsDone == false)
                 .ToArrayAsync();
         }
+<<<<<<< HEAD
+=======
+
+        public async Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            newItem.Id = Guid.NewGuid();
+            newItem.IsDone = false;
+            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+
+            _context.Items.Add(newItem);
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+        }
+>>>>>>> fa179d2 (Initial commit after making changes)
     }
 }
