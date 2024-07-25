05ee604ddcdac23cf5a54e0573c8eaea848f4901
mrivera <matias.g.rivera.civ@gmail.com>
2024-07-25 09:34:06 -0700

    Added Identity and Authorization (as of 79)

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,17 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Models;
 using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AspNetCoreTodo.Controllers
 {
+    [Authorize]
     public class TodoController : Controller
     {  
         private readonly ITodoItemService _todoItemService;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public TodoController(ITodoItemService todoItemService)
+
+        public TodoController(ITodoItemService todoItemService, UserManager<IdentityUser> userManager)
         {
             _todoItemService = todoItemService;
+            _userManager = userManager;
+        }
+
+        // Index block
+        public async Task<IActionResult> Index()
+        {
+            // Get todo items from database
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            // Put items into model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            // Render view using the model
+            return View(model);
         }
 
         // AddItem block
@@ -54,20 +77,9 @@ namespace AspNetCoreTodo.Controllers
             return RedirectToAction("Index");
         }
 
-        // Index block
-        public async Task<IActionResult> Index()
-        {
-            // Get todo items from database
-            var items = await _todoItemService.GetIncompleteItemsAsync();
+    }
 
-            // Put items into model
-            var model = new TodoViewModel()
-            {
-                Items = items
-            };
-
-            // Render view using the model
-            return View(model);
-        }
+    public class ApplicationUser
+    {
     }
 }
